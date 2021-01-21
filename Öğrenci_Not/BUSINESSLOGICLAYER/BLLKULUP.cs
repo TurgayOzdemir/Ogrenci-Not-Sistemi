6616057066757ae003f222b3b2979ac5f7ab8aeb
TurgayOzdemir <48651517+TurgayOzdemir@users.noreply.github.com>
2021-01-21 22:45:25 +0300

    Add files via upload

---
+++ Öğrenci_Not/BUSINESSLOGICLAYER/BLLKULUP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITYLAYERS;
+using FACADELAYERS;
+
+namespace BUSINESSLOGICLAYER
+{
+    public class BLLKULUP
+    {
+        public static int EKLE(ENTITYKULUP deger)
+        {
+            if (deger.KULUPAD != null)
+            {
+                return FACADEKULUP.EKLE(deger);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static bool GUNCELLE(ENTITYKULUP deger)
+        {
+            if(deger.KULUPAD!=null && deger.KULUPID != null)
+            {
+                return FACADEKULUP.GUNCELLE(deger);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool SIL(int deger)
+        {
+            if (deger != null)
+            {
+                return FACADEKULUP.SIL(deger);
+            }
+            return false;
+        }
+
+        public static List<ENTITYKULUP> LISTELE()
+        {
+            return FACADEKULUP.KULUPLISTESI();
+        }
+
+    }
+}
---
+++ Öğrenci_Not/BUSINESSLOGICLAYER/BLLOGRENCI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITYLAYERS;
+using FACADELAYERS;
+
+namespace BUSINESSLOGICLAYER
+{
+    public class BLLOGRENCI
+    {
+        public static int EKLE(ENTITYOGRENCI deger)
+        {
+            if(deger.AD!=null && deger.SOYAD!=null && deger.KULUPID!=null && deger.FOTOGRAF!=null && deger.KULUPID > 0)
+            {
+                return FACADEOGRENCI.EKLE(deger);
+            }
+            return -1;
+        }
+
+        public static bool GUNCELLE(ENTITYOGRENCI deger)
+        {
+            if (deger.AD != null && deger.SOYAD != null && deger.KULUPID != null && deger.FOTOGRAF != null && deger.KULUPID > 0 && deger.ID!=null)
+            {
+                return FACADEOGRENCI.GUNCELLE(deger);
+            }
+            return false;
+        }
+
+        public static bool SIL(int deger)
+        {
+            if(deger!=null && deger > 1)
+            {
+                return FACADEOGRENCI.SIL(deger);
+            }
+            return false;
+        }
+
+        public static List<ENTITYOGRENCI> LISTELE()
+        {
+            return FACADEOGRENCI.OGRENCILISTESI();
+        }
+
+    }
+}
